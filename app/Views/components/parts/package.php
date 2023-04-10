040858de629d01b6dd19fb91d0902277179c6a20
balbalcode <iqbalmaulana027@gmail.com>
2023-04-11 03:35:19 +0700

    chore: initial commit

---
+++ app/Views/components/parts/package.php
@@ -0,0 +1,275 @@
+
+<div class="">
+	<div class="container-lg px-5">
+		<div class="py-5 mx-auto text-center px-2">
+			<p class="fs-1 mt-3 fw-bold">
+				Cloud Hosting Indonesia <br> <strong class="text-primary"> Diskon Hingga 50%!</strong>
+			</p>
+			<p class="fw-normal">
+				Layanan cloud hosting indonesia terbaik dengan teknologi andal untuk website anda 
+			</p>			
+		</div>
+		<div class="d-flex px-5">
+			<div class="border border-light rounded shadow-sm mx-1 bg-white p-3">
+				<div class="text-center py-3">
+					<p class="my-0 text-dark fw-semibold small">
+						<small class="text-secondary">(VCH1)</small><br>
+						Value Cloud Hosting 
+					</p>
+					<p class="my-3 ">
+						<strong class="text-primary fs-4 fw-bolder">
+							Rp 14.500 <small class="fs-6">/ Bulan</small>
+						</strong>  <br>
+						<small>
+							<s class="text-secondary">Rp 25.000</s>
+						</small>
+					</p>
+				</div>
+				<div class="py-3">
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-hdd text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							Disk <strong>3 GB</strong>
+						</p>
+					</div>
+
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-wifi text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							Bandwith <strong>Unlimited</strong>
+						</p>
+					</div>
+
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-bolt-circle text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							CPU <strong>0.05 Core</strong>
+						</p>
+					</div>
+
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-check-shield text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							<strong>No</strong> Addon Domain
+						</p>
+					</div>
+
+					<p class="small text-secondary fs-7">
+						Paket cloud hosting murah berkualitas yang dirancang khusus untuk keperluan sederhana, seperti UKM, toko online, blog, website skala kecil, atau email untuk penggunaan personal. 
+					</p>
+
+					<button class="btn btn-primary rounded-pill w-100">Beli Sekarang</button>
+				</div>
+			</div>
+
+
+
+			<div class="border border-light rounded shadow mx-1 mt-n3 bg-white">
+				<div class="bg-primary text-white fw-bold rounded-top text-center p-2 fs-7">
+					Best Seller #1
+				</div>
+				<div class="px-3">
+					<div class="text-center py-3  px-3">
+						<p class="my-0 text-dark fw-semibold small">
+							<small class="text-secondary">(VCH1)</small><br>
+							Value Cloud Hosting 
+						</p>
+						<p class="my-3 ">
+							<strong class="text-primary fs-4 fw-bolder">
+								Rp 14.500 <small class="fs-6">/ Bulan</small>
+							</strong>  <br>
+							<small>
+								<s class="text-secondary">Rp 25.000</s>
+							</small>
+						</p>
+					</div>
+					<div class="py-3">
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-hdd text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								Disk <strong>3 GB</strong>
+							</p>
+						</div>
+
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-wifi text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								Bandwith <strong>Unlimited</strong>
+							</p>
+						</div>
+
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-bolt-circle text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								CPU <strong>0.05 Core</strong>
+							</p>
+						</div>
+
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-check-shield text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								<strong>No</strong> Addon Domain
+							</p>
+						</div>
+
+						<p class="small text-secondary fs-7">
+							Paket cloud hosting murah berkualitas yang dirancang khusus untuk keperluan sederhana, seperti UKM, toko online, blog, website skala kecil, atau email untuk penggunaan personal. 
+						</p>
+
+						<button class="btn btn-primary rounded-pill w-100">Beli Sekarang</button>
+					</div>
+				</div>
+				<div class="bg-dark text-white fw-bold rounded-bottom text-center p-2 fs-7">
+					Penawaran Terbaik!
+				</div>
+			</div>
+
+			<div class="border border-light rounded shadow mx-1 mt-n3 bg-white">
+				<div class="bg-primary text-white fw-bold rounded-top text-center p-2 fs-7">
+					Best Seller #1
+				</div>
+				<div class="px-3">
+					<div class="text-center py-3  px-3">
+						<p class="my-0 text-dark fw-semibold small">
+							<small class="text-secondary">(VCH1)</small><br>
+							Value Cloud Hosting 
+						</p>
+						<p class="my-3 ">
+							<strong class="text-primary fs-4 fw-bolder">
+								Rp 14.500 <small class="fs-6">/ Bulan</small>
+							</strong>  <br>
+							<small>
+								<s class="text-secondary">Rp 25.000</s>
+							</small>
+						</p>
+					</div>
+					<div class="py-3">
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-hdd text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								Disk <strong>3 GB</strong>
+							</p>
+						</div>
+
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-wifi text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								Bandwith <strong>Unlimited</strong>
+							</p>
+						</div>
+
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-bolt-circle text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								CPU <strong>0.05 Core</strong>
+							</p>
+						</div>
+
+						<div class="d-flex">
+							<p>
+								<i class='bx bx-xs bx-check-shield text-primary'></i>
+							</p>
+							<p class="ms-2 small">
+								<strong>No</strong> Addon Domain
+							</p>
+						</div>
+
+						<p class="small text-secondary fs-7">
+							Paket cloud hosting murah berkualitas yang dirancang khusus untuk keperluan sederhana, seperti UKM, toko online, blog, website skala kecil, atau email untuk penggunaan personal. 
+						</p>
+
+						<button class="btn btn-primary rounded-pill w-100">Beli Sekarang</button>
+					</div>
+				</div>
+				<div class="bg-dark text-white fw-bold rounded-bottom text-center p-2 fs-7">
+					Penawaran Terbaik!
+				</div>
+			</div>
+
+
+			<div class="border border-light rounded shadow-sm mx-1 bg-white p-3">
+				<div class="text-center py-3">
+					<p class="my-0 text-dark fw-semibold small">
+						<small class="text-secondary">(VCH1)</small><br>
+						Value Cloud Hosting 
+					</p>
+					<p class="my-3 ">
+						<strong class="text-primary fs-4 fw-bolder">
+							Rp 14.500 <small class="fs-6">/ Bulan</small>
+						</strong>  <br>
+						<small>
+							<s class="text-secondary">Rp 25.000</s>
+						</small>
+					</p>
+				</div>
+				<div class="py-3">
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-hdd text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							Disk <strong>3 GB</strong>
+						</p>
+					</div>
+
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-wifi text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							Bandwith <strong>Unlimited</strong>
+						</p>
+					</div>
+
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-bolt-circle text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							CPU <strong>0.05 Core</strong>
+						</p>
+					</div>
+
+					<div class="d-flex">
+						<p>
+							<i class='bx bx-xs bx-check-shield text-primary'></i>
+						</p>
+						<p class="ms-2 small">
+							<strong>No</strong> Addon Domain
+						</p>
+					</div>
+
+					<p class="small text-secondary fs-7">
+						Paket cloud hosting murah berkualitas yang dirancang khusus untuk keperluan sederhana, seperti UKM, toko online, blog, website skala kecil, atau email untuk penggunaan personal. 
+					</p>
+
+					<button class="btn btn-primary rounded-pill w-100">Beli Sekarang</button>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</div>
---
+++ app/Views/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QWords</title>
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;700&family=Secular+One&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <style type="text/css">
+       *{ font-family: 'Plus Jakarta Sans', sans-serif}
+   </style>
+   <body>
+
+   <div>
+      <?php include('components/core/header.php'); ?>
+      <?php include('components/parts/banner.php'); ?>
+      <?php include('components/parts/domain.php'); ?>
+      <?php include('components/parts/package.php'); ?>
+   </div>
+
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</head>
+</html>
